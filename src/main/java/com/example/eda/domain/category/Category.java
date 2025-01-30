f57e97225fb9d06423667f4c8744c67d5f3fc73c
Lucas Muniz <lucaslima1048@gmail.com>
2025-01-30 18:43:51 -0300

    add category entity

---
+++ src/main/java/com/example/eda/domain/category/Category.java
@@ -0,0 +1,80 @@
+package com.example.eda.domain.category;
+
+
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@DynamoDbBean
+public class Category {
+
+    private String id;
+
+    private String title;
+
+    private String ownerId;
+
+    private String description;
+
+    private Instant createdAt;
+
+    @DynamoDbPartitionKey
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @DynamoDbAttribute("title")
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @DynamoDbAttribute("owner_id")
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    @DynamoDbAttribute("description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @DynamoDbAttribute("created_at")
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id) && Objects.equals(title, category.title) && Objects.equals(ownerId, category.ownerId) && Objects.equals(description, category.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, ownerId, description);
+    }
+}
