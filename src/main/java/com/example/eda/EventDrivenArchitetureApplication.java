22ac7f438ac45dde51b4b4993dfea07cbc302d02
Lucas Muniz <lucaslima1048@gmail.com>
2025-01-30 18:23:00 -0300

    first commit

---
+++ src/main/java/com/example/eda/EventDrivenArchitetureApplication.java
@@ -0,0 +1,13 @@
+package com.example.eda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenArchitetureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenArchitetureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eda/EventDrivenArchitetureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenArchitetureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
