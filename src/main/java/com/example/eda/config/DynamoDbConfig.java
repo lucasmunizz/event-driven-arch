7c1d90c6aa3dd3efd8229f3c37b5c11f743e00ea
Lucas Muniz <lucaslima1048@gmail.com>
2025-01-30 18:33:55 -0300

    add dynamo config

---
+++ src/main/java/com/example/eda/config/DynamoDbConfig.java
@@ -0,0 +1,19 @@
+package com.example.eda.config;
+
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import java.net.URI;
+
+@Configuration
+public class DynamoDbConfig {
+
+    public DynamoDbClient dynamoDbClient(){
+        return DynamoDbClient.builder()
+                .endpointOverride(URI.create("http://localhost:4566"))
+                .region(Region.US_EAST_1)
+                .build();
+    }
+
+}
